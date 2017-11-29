f4c5e23df0929b8c6545f7214ff41eda7d4fc4df
Juan Callejas <jcallejasrodriguez@gmail.com>
2017-11-29 00:20:18 -0400

    Added basic navigation

---
+++ Assets/Scripts/Floorplan.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Floorplan : MonoBehaviour {
+
+	public bool First;
+
+	public Transform[] Waypoints;
+
+	public Floorplan next;
+	public Floorplan prev;
+
+	private Transform StartPos;
+	private Transform EndPos;
+
+	public static Floorplan FirstFloorplan;
+
+	// Use this for initialization
+	void Start () {
+
+		if (!Application.isPlaying)
+			return;
+		
+		//Get the start and end points of this floorplan
+		foreach (Transform pos in transform.GetChild(0).GetComponentInChildren <Transform>()) {
+			if (pos.name == "Start")
+				StartPos = pos;
+			else if (pos.name == "End")
+				EndPos = pos;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Render lines and rays for in-editor flow visualization
+		for (int i = 0; i < Waypoints.Length; i++){
+			Debug.DrawRay(Waypoints[i].position, Waypoints[i].right * 0.5f, Waypoints[i].name.Contains ("Skip") ? Color.grey : Color.blue);
+			Debug.DrawRay(Waypoints[i].position, -Waypoints[i].up * 2, Waypoints[i].name.Contains ("Skip") ? Color.grey : Color.blue);
+			if (i < Waypoints.Length - 1)
+				Debug.DrawLine(Waypoints[i].position, Waypoints[i + 1].position, Color.green);
+		}
+		//Lines 
+		if (prev)
+			Debug.DrawLine(Waypoints[0].position, prev.Waypoints[prev.Waypoints.Length - 1].position, Color.yellow);
+
+		//Set FirstFloorplan
+		if (First) {
+			FirstFloorplan = this;
+			First = false;
+		}
+
+		//Sync prev/next floorplans
+		if (next)
+			next.prev = this;
+
+		//Check during editmode if there is no StarPos or EndPos
+		if (!StartPos || !EndPos) {
+			//Get the start and end points of this floorplan
+			foreach (Transform pos in transform.GetChild(0).GetComponentInChildren<Transform>()) {
+				if (pos.name == "Start")
+					StartPos = pos;
+				else if (pos.name == "End")
+					EndPos = pos;
+			}
+			//Check if both endpoints were found during the last pass, warn the user otherwise
+			if (!StartPos || !EndPos)
+				if (!StartPos && EndPos)
+					Debug.LogError("Cound not find StartPos");
+				else if (StartPos && !EndPos)
+					Debug.LogError("Could not find EndPos");
+				else
+					Debug.LogError("Could not find StarPos nor EndPos");
+			
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/ViewerControl.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewerControl : MonoBehaviour {
+
+	public float LerpSpeed;
+
+	public Floorplan CurrentFloorplan;
+	public int CurrentImage;
+	public int lastDir;
+
+	// Use this for initialization
+	void Start () {
+
+		CurrentFloorplan = Floorplan.FirstFloorplan;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetButtonDown ("Prev"))
+            PrevImage();
+		else if (Input.GetButtonDown ("Next"))
+            NextImage();
+
+		if (!CurrentFloorplan)
+			CurrentFloorplan = Floorplan.FirstFloorplan;
+
+		/* 
+		 * Update the viewer location.
+         * This is called every frame 
+         * but is its own method to 
+         * keep the main Update() 
+         * method clean. 
+		*/
+		UpdateLocation();
+		
+	}
+
+	private void UpdateLocation () {
+
+		Transform destination = CurrentFloorplan.Waypoints[CurrentImage];
+
+		//What do we do if we finish moving tp the destination
+		if (isSame(transform.position, destination.position, 0.5f))
+			//If the destination waypoint is simply a skippable transition, proceed to the next waypoint
+			if (destination.name.Contains("Skip"))
+				if (lastDir > 0)
+					NextImage();
+				else
+					PrevImage();
+
+		if (isSame(transform.position, destination.position, 0.00001f))
+			return;
+
+		transform.position = Vector3.Lerp(transform.position, destination.position, Time.deltaTime * LerpSpeed * 10);
+		transform.rotation = Quaternion.Lerp(transform.rotation, CurrentFloorplan.Waypoints[CurrentImage].rotation, Time.deltaTime * LerpSpeed * 10);
+
+	}
+
+	private void PrevImage() {
+		//If we're not on the first image, simply decrease the image index
+		if (CurrentImage > 0)
+			CurrentImage--;
+		//If we're on the first image, take us to the previous floorplan and make the image index the last image
+		else {
+			if (CurrentFloorplan.prev) {
+				CurrentFloorplan = CurrentFloorplan.prev;
+				CurrentImage = CurrentFloorplan.Waypoints.Length - 1;
+			} else
+				//TODO Add proper UI for this scenario
+				Debug.Log("First image, cannot reverse");
+		}
+		lastDir = -1;
+	}
+
+	private void NextImage() {
+		//If we're not on the last image, simply increase the image index
+		if (CurrentImage < CurrentFloorplan.Waypoints.Length - 1)
+			CurrentImage++;
+		
+		//If we're on the last image, take us to the next floorplan and reset the image index
+		else {
+			if (CurrentFloorplan.next) {
+				CurrentFloorplan = CurrentFloorplan.next;
+				CurrentImage = 0;
+			} else
+				//TODO Add proper UI for this scenario
+				Debug.Log("Last image, cannot proceed");
+		}
+		lastDir = 1;
+	}
+
+	public bool isSame(Quaternion val1, Quaternion val2, float f) {
+		return isSame(val1.eulerAngles, val2.eulerAngles, f);
+	}
+
+	public bool isSame (Vector3 val1, Vector3 val2, float f) {
+		return isSame(val1.x, val2.x, f) && isSame(val1.y, val2.y, f) && isSame(val1.z, val2.z, f);
+	}
+
+	public bool isSame (float val1, float val2, float f) {
+		return Mathf.Abs(val1 - val2) < f;
+	}
+
+}
